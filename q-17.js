c6e3b4d5c6b7e7c57964617226f43c4fe5e9bca5
hadiakhan785 <hadiakhan785@gmail.com>
2018-06-06 11:32:44 +0500

    Final Commit

---
+++ q-17.js
@@ -0,0 +1,9 @@
+let mathTestScore = 15;
+let computerTestScore = 21;
+let englishTestScore = 9;
+
+if (mathTestScore > 10 && computerTestScore > 20 && englishTestScore > 8){
+    console.log ("This student passed the admission test");
+} else {
+    console.log ("This student failed");
+}
